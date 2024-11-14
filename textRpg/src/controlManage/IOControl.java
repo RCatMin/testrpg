d05f93117475798568477c06d52a42995729b58f
RCatMin <razzcat_min@naver.com>
2024-11-14 16:15:06 +0900

    Create IOManager class to store I/O variables
    Add printString() method

---
+++ textRpg/src/controlManage/IOControl.java
@@ -0,0 +1,36 @@
+package controlManage;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class IOControl {
+	public static StringBuilder buffer;
+	public static BufferedWriter writer;
+	private BufferedReader reader;
+
+	private IOControl() {
+		buffer = new StringBuilder();
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		reader = new BufferedReader (new InputStreamReader(System.in));
+	}
+	
+	public static IOControl instance = new IOControl();
+	
+	public static IOControl getInstance() {
+		return instance;
+	}
+	
+	public static void printString(String message) {
+		buffer.setLength(0);
+		buffer.append(message);
+		try {
+			writer.append(buffer);
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
