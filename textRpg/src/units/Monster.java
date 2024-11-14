8bd04a825d452ca6d5e07c44c3241ad68ad3e031
RCatMin <razzcat_min@naver.com>
2024-11-14 17:15:57 +0900

    Create monster class and Define variables

---
+++ textRpg/src/units/Monster.java
@@ -0,0 +1,40 @@
+package units;
+
+import controlManage.IOControl;
+
+public class Monster {
+
+	int hp; // 체력
+	int maxHp; // 최대 체력
+	int power; // 공격력
+	String monsterName; // 몬스터 이름
+	String type; // 몬스터 등급
+
+	Monster(){
+		
+	}
+	
+	public Monster (String monsterName, int maxHp, int power, String type) {
+		this.monsterName = monsterName;
+		this.hp = maxHp;
+		this.maxHp = maxHp;
+		this.power = power;
+		this.type = type;
+	}
+	
+	public Monster (int maxHp, int power, String type) {
+		this.hp = maxHp;
+		this.maxHp = maxHp;
+		this.type = type;
+	}
+	
+	
+	public void attack (Unit target) {
+		
+	}
+	
+	public void printMonster() {
+		String info = String.format("체력 : %3d / %3d\n등급 : %2s", hp, maxHp, type);
+		IOControl.printString(info);
+	}
+}
