7440fd9f3a52835494a5d2e4f9b4e3802df2b2c2
RCatMin <razzcat_min@naver.com>
2024-11-14 17:54:29 +0900

    Modified the Game Manager Class to handle startup and shutdown.

--- textRpg/src/textRpg/GameManager.java
+++ textRpg/src/textRpg/GameManager.java
@@ -1,5 +1,6 @@
 package textRpg;
 
+import controlManage.IOControl;
 
 public class GameManager {
 	private GameManager() {
@@ -13,7 +14,22 @@ public class GameManager {
 	}
 	
 	public void run() {
-		
+		start();
+		String input = IOControl.inputString("\n여기에 입력하세요 : ");
+		if (input.equals("시작")) {
+			
+		} else {
+			end();
+			return;
+		}
+	}
+	
+	private void start() {
+		IOControl.printString("게임을 시작하려면 '시작' 이라고 입력하세요\n아니면 아무 키를 입력하세요");
+	}
+	
+	private void end() {
+		IOControl.printString("게임을 종료합니다.");
 	}
 
 }
