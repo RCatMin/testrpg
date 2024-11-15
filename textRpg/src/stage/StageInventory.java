76c4c6c18357b55d29d064d077aa85ab7bc3ceb6
RCatMin <razzcat_min@naver.com>
2024-11-15 16:21:39 +0900

    To ensure smooth inventory processing, a new inventory package was created and processing was added through HashMap.

--- textRpg/src/stage/StageInventory.java
+++ textRpg/src/stage/StageInventory.java
@@ -1,9 +1,55 @@
 package stage;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import controlManage.IOControl;
+import inventory.EnhanceItem;
+import inventory.EquipItem;
+import inventory.SellItem;
+
 public class StageInventory implements Stage{
 
+	Map<String, inventory.Inventory> inventoryMenu;
+	Map<String, Stage> stageMenu;
+	
+	
+	public StageInventory() {
+		inventoryMenu = new HashMap<String, inventory.Inventory>();
+		inventoryMenu.put("장비착용", new EquipItem());
+		inventoryMenu.put("장비판매", new SellItem());
+		inventoryMenu.put("장비강화", new EnhanceItem());
+	}
+	
+	public void CheckBack() {
+		stageMenu = new HashMap<String, Stage>();
+		stageMenu.put("뒤로가기", (Stage) new StageLobby());
+	}
+	
 	public void activate() {
-		System.out.println ("인벤토리");
+		inventoryMenu();
+		while (true) {
+			String inputMenu = IOControl.inputString("\n여기에 입력하세요 : ");
+			if (inventoryMenu.containsKey(inputMenu)) {
+				inventoryMenu.get(inputMenu).activate();
+			} 
+			
+			if (stageMenu.containsKey(inputMenu)) {
+				stageMenu.get(inputMenu).activate();
+			}
+		}
+	}
+	
+	public void inventoryMenu() {
+		String message = """
+				============================================
+									메인 로비
+			
+				[장비 착용]	[장비 판매]	[장비 강화]	[뒤로가기]
+				
+				============================================
+				""";
+		IOControl.printString(message);
 	}
 
 }
